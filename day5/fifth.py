d3522c01cf05c14e4717c805a40ffd297c67279b
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2021-01-19 18:39:22 +0530

    Create fifth.py

---
+++ day5/fifth.py
@@ -0,0 +1,16 @@
+inp = open("input.txt", "r")
+arr =[]
+
+for each in inp:
+    arr.append(each)
+#lambda to remove \n from each
+remove = list(map(lambda a: a.strip("\n"), arr))
+seat_ids = []
+for each in remove:
+    d = each[:7]
+    e = each[7:]
+    ans1 = d.replace('F', '0').replace('B', '1')
+    ans2 = e.replace('L','0').replace('R', '1')
+    seat_ids.append(int(ans1,2)*8+int(ans2,2))
+print(max(seat_ids))
+
